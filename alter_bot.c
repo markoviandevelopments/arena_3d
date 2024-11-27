c05edf0088ffa3b4c2ae236f3192beea9345a8af
Presotn Brubaker <brubakerpreston@gmail.com>
2024-11-26 22:20:20 -0600

    m

--- alter_bot.c
+++ alter_bot.c
@@ -4,17 +4,19 @@ int counter = 0;
 
 void AlterBot(float *data) {
 
+    float sens = 1.0f
+
     if (data[8] == 1.0f && !counter) {
-        data[9] += 0.1f;
+        data[9] += sens;
         counter += 10;
     } else if (data[8] == 2.0f && !counter) {
-        data[10] -= 0.1f;
+        data[10] -= sens;
         counter += 10;
     } else if (data[8] == 3.0f && !counter) {
-        data[9] -= 0.1f;
+        data[9] -= sens;
         counter += 10;
     } else if (data[8] == 4.0f && !counter) {
-        data[10] += 0.1f;
+        data[10] += sens;
         counter += 10;
     }
 
