b70f3fae669c389762a2d76bb38ac61afec7e14f
Presotn Brubaker <brubakerpreston@gmail.com>
2024-11-27 22:27:28 -0600

    m

--- teleports.c
+++ teleports.c
@@ -18,12 +18,12 @@ void Teleports(Player *player) {
     }
 
     if (z < 972.0f && z > 971.5f && x < 1023.5f && x > 1008.0f && y > 0.0f && y < 20.0f) {
-        player->position.x = 0.0f;
-        player->position.z = 0.0f;
+        player->position.x = -83.1f;
+        player->position.z = -39.8f;
         player->position.y = 1.1f;
         player->velocityY = 0.0f;
-        player->yaw = 60.0f;
-        player->pitch = 0.0f;
+        player->yaw = 181.4f;
+        player->pitch = 64.65f;
     }
 
 
